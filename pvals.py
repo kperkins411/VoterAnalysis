3b44df97e979add9e09d2bea7e571fc51d15bfda
Keith Perkins <kperkins411@gmail.com>
2019-07-13 23:38:14 -0400

    pval calculations

--- pvals.py
+++ pvals.py
@@ -33,8 +33,12 @@ class pValInfo():
         non permuted pred
         '''
         # if either of the above are empty throws exception
-        n = sum(i > self.col for i in self.permuted_preds)
-        return (n / len(self.permuted_preds))
+        n = sum(i > self.correct_pred for i in self.permuted_preds)
+        val= (n / len(self.permuted_preds)).item(0)
+
+        #cheesy but it doesn't matter if they are all above or below
+        val=val if val<0.5 else (1-val)
+        return val
 
 def get_all_pvals(all_columns, trn, trn_y, tst, numb_iter):
     '''
@@ -48,7 +52,9 @@ def get_all_pvals(all_columns, trn, trn_y, tst, numb_iter):
     '''
     res = []
     for col in all_columns:
-        res.append(get_col_pval(col, trn, trn_y, tst, numb_iter))
+        trncpy = trn.copy()
+        res.append(get_col_pval(col, trncpy, trn_y, tst, numb_iter))
+    return res
 
 def get_col_pval(col, trn, trn_y, tst, numb_iter):
     '''
@@ -60,17 +66,16 @@ def get_col_pval(col, trn, trn_y, tst, numb_iter):
     :param numb_iter:  how many AMEs to generate on permuted data, used to produce prediction normal distribution
     :return: numbiter AME calcs
     '''
-    val = pValInfo()    #holder
-    val.correct_preds = get_AME(col, trn, trn_y, tst, numb_iter=numb_iter)
+    val = pValInfo(col)    #holderS
+
+    val.correct_pred = get_AME(col, trn, trn_y, tst, numb_iter=1)
+    print(f'correct get_AME for column {col} is {val.correct_pred[0]}')
 
     #permute column
-    cpy=trn[col].copy() #save for later
     trn[col]=np.random.permutation(trn[col].values)
 
+    print(f'get permuted get_AME for column {col}')
     val.permuted_preds = get_AME(col, trn, trn_y, tst, numb_iter=numb_iter)
-
-    #replace permuted column
-    trn[col] = cpy
 
     return val
 
@@ -85,6 +90,7 @@ def get_AME(col, trn, trn_y, tst, numb_iter):
     :return: numbiter AME calcs
     '''
     res = []
+    cnt=0
     for _ in range(numb_iter):
         # create and train a random forest object
         m_rf = RandomForestClassifier(n_estimators=NUMB_ESTIMATORS, n_jobs=-1, oob_score=True, max_features='auto',
@@ -103,12 +109,16 @@ def get_AME(col, trn, trn_y, tst, numb_iter):
 
             tst1[col].apply(lambda x: min if x == max else max)
         else:
-            tst1[col] += 1
+            tst1[col] += 1.0
 
         prob_new = ((m_rf.predict_proba(tst1))[:,0]).sum()/len(tst1)
 
         res.append(prob_new - prob_orig)
 
+        cnt=(cnt+1)%10
+        if (cnt==0):
+            print(".",end='')
+    print()
     return res
 
 
